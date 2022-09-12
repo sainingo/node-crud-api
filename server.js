6ea23a702ef9b862882b0bef89bbf522f0005c2d
sainingo <nickmentor421@gmail.com>
2022-09-12 21:35:25 +0300

    Add necessary dependecies

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+
+import useRoutes from './routes/users';
 
 const app = express();
+
+app.use('/users', useRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello there node');
