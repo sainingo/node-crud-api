fd2aab8ff209e5e6caf5a3bc355025b6ceac079f
sainingo <nickmentor421@gmail.com>
2022-09-12 22:19:38 +0300

    Get user with specific id

--- controllers/users.js
+++ controllers/users.js
@@ -12,3 +12,10 @@ export const createUser = (req, res) => {
     users.push({...user, id: uuidv4()})
     res.send(`User with the name ${user.firstName} added to the db`);
 }
+
+
+export const getUser = (req, res) => {
+    const { id } = req.params;
+    const user = users.find((user) => user.id === id);
+    res.send(user)
+}
